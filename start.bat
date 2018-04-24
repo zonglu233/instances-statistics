d6e3abc821d64a514d65edb7aec9bbb6459e9975
zonglu <415390588@qq.com>
2018-04-24 13:19:26 +0800

    提交

--- start.bat
+++ start.bat
@@ -1,13 +1,12 @@
 rem ##### localhost
-rem set ROOT_URL=http://127.0.0.1:3089/
 rem set DB_SERVER=localhost
 
 rem ##### 21 server
-set ROOT_URL=http://127.0.0.1:3089/
 set DB_SERVER=192.168.0.21
 
 set MONGO_URL=mongodb://%DB_SERVER%/steedos
 set MONGO_OPLOG_URL=mongodb://%DB_SERVER%/local
 set MULTIPLE_INSTANCES_COLLECTION_NAME=workflow_instances
 
+set ROOT_URL=http://127.0.0.1:3089/
 meteor run --port 3089 --settings settings.json
